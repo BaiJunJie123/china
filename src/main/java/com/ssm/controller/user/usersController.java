33f441113fd7649bb65cf9b0623e385ceac3bb46
baijunjie4 <2604420325@qq.com>
2019-03-12 03:22:38 +0800

    new tijiao

--- src/main/java/com/ssm/controller/user/usersController.java
+++ src/main/java/com/ssm/controller/user/usersController.java
@@ -30,53 +30,47 @@ public class usersController {
     @RequestMapping("/login.html")
    
 	public String login() {
-    	String url = "";
-    	String name ="sw";
-    	String pass = "sw";
-    	UsernamePasswordToken token = new UsernamePasswordToken(name,pass);
-		token.setRememberMe(true);
-	    System.out.println("为了验证登录用户而封装的token为" + ReflectionToStringBuilder.toString(token, ToStringStyle.MULTI_LINE_STYLE));  
-    	Subject subject = SecurityUtils.getSubject();
-    	 System.out.println("对用户[" + name + "]进行登录验证..验证开始"); 
-    	 try {
-    		 subject.login(token);
-    		 System.out.println("对用户[" + name + "]进行登录验证..验证通过");  
-    	 }catch(Exception e) {
-    		 e.printStackTrace();
-    		 return "userjsp/error";
-    	 }
-    	
-    	 
-    	if(subject.isAuthenticated()) {
-    		 System.out.println("用户[" + name + "]登录认证通过(这里可以进行一些认证通过后的一些系统参数初始化操作)"); 
-    		 url = "userjsp/success";
-    	}else {
-    		token.clear();
-    	}
-    	System.out.println("good login");
+          
 		/*
 		 * user uu = new user(); uu.setId(86101); List<user> ji =
 		 * userService.findusers(uu); for(user s : ji) {
 		 * System.out.println(s.getAgent_status()+"======================"); }
 		 */
 		
-		return url;
+		return "userjsp/login";
 	}
     @RequestMapping("/success.html")
     
- 	public String login2() {
+ 	public String login2(@RequestParam("name") String name,@RequestParam("pass") String pass) {
  		System.out.println("good success");
+ 		try {
+ 			 Subject subject = SecurityUtils.getSubject();
+ 	 	     UsernamePasswordToken token = new UsernamePasswordToken(name,pass);
+			/* token.setRememberMe(true); */
+ 	 		 subject.login(token);
+ 	 		System.out.println( subject.hasRole("admin")+"是否有这个角色");
+ 	 		 System.out.println("Contorller验证成功");
+ 	 		return "userjsp/success";
+ 	 		
+ 		}catch(Exception e) {
+ 			 System.out.println("Contorller验证失败");
+ 			return "userjsp/error";
+ 			
+ 		}
+ 	    
  		/*
  		 * user uu = new user(); uu.setId(86101); List<user> ji =
  		 * userService.findusers(uu); for(user s : ji) {
  		 * System.out.println(s.getAgent_status()+"======================"); }
  		 */
- 		return "userjsp/success";
+ 	//	return "userjsp/success";
  	}
  @RequestMapping("/error.html")
     
  	public String login3() {
- 		System.out.println("good success");
+ 		System.out.println("good error");
+ 		Subject subject = SecurityUtils.getSubject();
+ 		subject.logout();
  		/*
  		 * user uu = new user(); uu.setId(86101); List<user> ji =
  		 * userService.findusers(uu); for(user s : ji) {
